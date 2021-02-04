457ffcb8ae593c3ca19b98202e0cc94b194410ac
ernscht <ernscht@gmail.com>
2021-02-04 18:29:51 +0100

    chore: update dev deps & default node version

--- dist/lib.js
+++ dist/lib.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = global || self, factory(global.preventWindowOpenAttacks = {}));
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.preventWindowOpenAttacks = {}));
 }(this, (function (exports) { 'use strict';
 
   function preventWindowOpenerAttacks(clickEvent) {
